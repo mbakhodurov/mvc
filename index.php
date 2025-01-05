1de6334af0f7210790847aaee04c59a287534777
mbakhodurov <mbakhodurov@gmail.com>
2025-01-05 11:57:33 +0500

    3

--- index.php
+++ index.php
@@ -1,10 +1,10 @@
 <?php
 require_once __DIR__.'/vendor/autoload.php';
 use app\core\Application;
+
+$app = new Application();
 echo "Fdas";
 exit;
-$app = new Application();
-
 //$app->router->get('/', function (){
 //    return "Hello world";
 //});
